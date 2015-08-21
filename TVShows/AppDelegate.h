e2535dba5156de8f2dc709728961e2bf0d79ba8e
Raghav Sai Cheedalla <raghavsaicheedalla@Raghavs-MacBook-Pro.local>
2015-08-21 08:54:16 -0700

    Initial Commit

---
+++ TVShows/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TVShows
+//
+//  Created by Raghav Sai Cheedalla on 8/21/15.
+//  Copyright (c) 2015 Raghav Sai Cheedalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TVShows/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TVShows
+//
+//  Created by Raghav Sai Cheedalla on 8/21/15.
+//  Copyright (c) 2015 Raghav Sai Cheedalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
